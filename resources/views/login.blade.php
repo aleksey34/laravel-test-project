db6da179ae6ea61de8d6e2d537b3ead16a2ee770
aleksey34 <olga.vlad@inbox.ru>
2021-11-15 19:50:08 +0300

    some change in ligin.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -70,6 +70,14 @@
     @endif
 {{--scripts--}}
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
+<script>
+    const test = 4;
+    if(test -  parseInt(test)){
+        // alert('float' );
+    }else{
+        // alert('number')
+    }
+// alert(test%1);
+</script>
     </body>
 </html>
